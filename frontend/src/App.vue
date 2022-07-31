54c121248459c2cd3b86447b31b7005cadd24be4
chandrafebrian <chandrafebrian99@gmail.com>
2022-07-31 19:15:19 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@
         ></v-img>
       </template>
 
-      <v-toolbar-title>Todo App Docker Chandra</v-toolbar-title>
+      <v-toolbar-title>Todo App Chandra</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -19,4 +19,4 @@
 
 <script>
 export default {};
-</script>
+</script> 
